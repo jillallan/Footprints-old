77eb038e449e4582ae601a2aaa21eb6b4283a4fb
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-04-26 14:19:45 +0100

    Add Equal Width HStack

---
+++ Footprints/Containers/DynamicGridContainer.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicGridContainer.swift
+//  Footprints
+//
+//  Created by Jill Allan on 26/04/2024.
+//
+
+import SwiftUI
+
+struct DynamicGridContainer: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        .zero
+    }
+
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        
+    }
+}
