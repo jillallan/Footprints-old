8e113b4423773d8c060733c767c178333b1b6b10
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-05-04 13:57:11 +0100

    Updated trip view and trip card

---
+++ Footprints/Extensions/OSLog+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  OSLog+Extension.swift
+//  Footprints
+//
+//  Created by Jill Allan on 03/05/2024.
+//
+
+import Foundation
---
+++ Footprints/Trips/TripCardLargeText.swift
@@ -0,0 +1,18 @@
+//
+//  TripCardLargeText.swift
+//  Footprints
+//
+//  Created by Jill Allan on 04/05/2024.
+//
+
+import SwiftUI
+
+struct TripCardLargeText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripCardLargeText()
+}
