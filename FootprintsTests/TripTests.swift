2501b658681977b7fa65b02d7fdd5f9347cf96d7
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-05-13 22:05:35 +0100

    Add test for Edit trip view

--- FootprintsTests/TripTests.swift
+++ FootprintsTests/TripTests.swift
@@ -9,20 +9,6 @@ import XCTest
 @testable import Footprints
 
 final class TripTests: ModelContainerTestCase {
-    var dateFormatter: DateFormatter!
 
-    @MainActor override func setUpWithError() throws {
-        try super.setUpWithError()
-
-        dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    }
-
-    func testTrip_initialiseTripFromTrip_createsMatchingTrip() {
-        
-    }
-
-    func testTrip_equatbleConformance_canCompareTwoTripsForEquailty() {
-
-    }
+    
 }
--- FootprintsUITests/TripViewUITests.swift
+++ FootprintsUITests/TripViewUITests.swift
@@ -26,27 +26,6 @@ final class TripViewUITests: XCTestCase {
         try app.performAccessibilityAudit()
     }
 
-    func testTripView_whenIphonePotrait_TripsShownInOneColumn() {
-        print(app.debugDescription)
-        let scrollView = app.scrollViews
-            .element(boundBy: 0)
-            .children(matching: .other)
-            .element(boundBy: 0)
-
-        let scrollViewWidth = scrollView.frame.width
-
-        let images = app.scrollViews.descendants(matching: .image)
-        let imageWidths = images.allElementsBoundByIndex.map(\.frame.height)
-
-        let averageWidth = imageWidths.reduce(0, +) / Double(imageWidths.count)
-        XCTAssertEqual(
-            scrollViewWidth,
-            averageWidth,
-            accuracy: 100.0,
-            "scroll view width \(scrollViewWidth) should be approx equal to avg width of images: \(averageWidth)"
-        )
-    }
-
     func testTripView_whenIphoneLandscape_TripsShownInTwoColumns() {
         print(app.debugDescription)
 #if !os(macOS)
@@ -71,33 +50,50 @@ final class TripViewUITests: XCTestCase {
         )
     }
 
-    func testTripView_whenIpadPotrait_TripsShownInTwoColumns() {
-        print(app.debugDescription)
-        let scrollView = app.scrollViews
-            .element(boundBy: 0)
-            .children(matching: .other)
-//            .children(matching: .other)
-            .element(boundBy: 0)
-
-        let scrollViewWidth = scrollView.frame.width
-
-        let images = app.scrollViews.descendants(matching: .image)
-        let imageWidths = images.allElementsBoundByIndex.map(\.frame.height)
-
-        let averageWidth = imageWidths.reduce(0, +) / Double(imageWidths.count) * 2
-        XCTAssertEqual(
-            scrollViewWidth,
-            averageWidth,
-            accuracy: 100.0,
-            "scroll view width \(scrollViewWidth) should be approx equal to avg width of images: \(averageWidth)"
-        )
-    }
-
     func testTripView_AddingATrip_ShowsNewTripinTripDetailView() {
+        
+        let tripButton = app
+            .scrollViews
+            .otherElements
+            .buttons
+            .element(boundBy: 0)
+        XCTAssert(tripButton.isHittable)
+        tripButton.tap()
+
+        let editButton = app
+            .navigationBars
+            .element(boundBy: 0)
+            .buttons["Edit"]
+        XCTAssert(editButton.isHittable)
+        editButton.tap()
+
+        let tripTitleTextField = app
+            .collectionViews
+            .textFields["Name your trip"]
+        XCTAssert(tripTitleTextField.isHittable)
+        tripTitleTextField.tap()
+
+        tripTitleTextField.clearAndEnterText(text: "New Title")
+
+        let saveButton = app
+            .navigationBars
+            .element(boundBy: 0)
+            .buttons["Save"]
+        XCTAssert(saveButton.isHittable)
+        saveButton.tap()
+
+//        let tripsButton = app
+//            .navigationBars.element(boundBy: 0).buttons["Trips"]
+//        XCTAssert(tripsButton.isHittable)
+//        tripsButton.tap()
+
+        let tripTitle = app.navigationBars["New Title"]
+        XCTAssert(tripTitle.exists)
 
     }
 
     func testTripView_AddingATrip_NewTripShowsInTripView() {
-
+        
+        
     }
 }
