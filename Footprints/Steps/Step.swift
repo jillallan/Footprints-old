b30a86e5c29dbeff52031fde4a5c5d46fc33b71f
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-05-20 22:35:32 +0100

    Add documentation for Step model close #15

--- Footprints/Steps/Step.swift
+++ Footprints/Steps/Step.swift
@@ -24,6 +24,12 @@ final class Step {
         """
     }
 
+    /// Initializes a new instance of a Step
+    /// - Parameters:
+    ///   - timestamp: The time of the step, the defualt is the current time
+    ///   - latitude: The latitude of the step
+    ///   - longitude: The longitude of a step
+    ///   - altitude: The altitude of a step
     init(timestamp: Date = .now, latitude: Double, longitude: Double, altitude: Double) {
         self.timestamp = timestamp
         self.latitude = latitude
@@ -31,10 +37,19 @@ final class Step {
         self.altitude = altitude
     }
 
+
+    /// Initializes a new instance of a Step, with a default altitude of 0.0
+    /// - Parameters:
+    ///   - timestamp: The time of the step, the defualt is the current time
+    ///   - latitude: The latitude of the step
+    ///   - longitude: The longitude of a step
     convenience init(timestamp: Date = .now, latitude: Double, longitude: Double) {
         self.init(timestamp: timestamp, latitude: latitude, longitude: longitude, altitude: 0.0)
     }
 
+
+    /// Initializes a new instance of a Step, with a default latitude, longtiude and altitude of 0.0
+    ///   - timestamp: The time of the step, the defualt is the current time
     convenience init(timestamp: Date = .now) {
         self.init(timestamp: timestamp, latitude: 0.0, longitude: 0.0, altitude: 0.0)
     }
