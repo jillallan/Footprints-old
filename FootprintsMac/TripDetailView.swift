ff19b978a1776123ab48919fdeb5d40f2d7fe0b4
Jill Allan <54241079+jillallan@users.noreply.github.com>
2024-05-20 16:00:05 +0100

    Added Mac target and updated code for mac

---
+++ FootprintsMac/TripDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TripDetailView.swift
+//  FootprintsMac
+//
+//  Created by Jill Allan on 20/05/2024.
+//
+
+import SwiftUI
+
+struct TripDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripDetailView()
+}
